098fead76d50dce3de8c021a7a6c299bc7b02ab3
ethan-Angumya <129429783+ethan-Angumya@users.noreply.github.com>
2025-04-11 18:52:15 +0300

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,12 +1,15 @@
+%%writefile data/dataset.py
 import os
-from torch.utils.data import Dataset
 from PIL import Image
+from torch.utils.data import Dataset
 
 class UAVDepthDataset(Dataset):
-    def __init__(self, image_dir, transform=None):
-        self.image_dir = image_dir
+    def __init__(self, image_dir=None, transform=None):
+        # Default to Colab path if not specified
+        self.image_dir = image_dir or 'data/images'
         self.transform = transform
-        self.image_files = [f for f in os.listdir(image_dir) if f.endswith(('.jpg', '.png'))]
+        self.image_files = [f for f in os.listdir(self.image_dir) 
+                          if f.endswith(('.jpg', '.png', '.jpeg'))]
         
     def __len__(self):
         return len(self.image_files)
@@ -14,8 +17,6 @@ class UAVDepthDataset(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.image_dir, self.image_files[idx])
         image = Image.open(img_path).convert('RGB')
-        
         if self.transform:
             image = self.transform(image)
-            
         return image, self.image_files[idx]
